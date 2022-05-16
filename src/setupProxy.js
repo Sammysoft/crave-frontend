856dea595291b81f7572570fa9988648d4830529
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2022-05-16 23:59:09 +0000

    little changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:6069/api/',
+      target: 'https://api-crave.herokuapp.com/api/',
       changeOrigin: true,
     })
   );
